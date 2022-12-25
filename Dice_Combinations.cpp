e17b7b87a47bfb8c8192d2b6bba4dddda2325adb
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2022-12-25 20:12:10 +0530

    Add files via upload

---
+++ Dice_Combinations.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+                    
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long> vl;
+#define all(x) begin(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+typedef pair<long,long> vll;
+typedef pair<int,int> pi;
+                               
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Sieve of Eratosthenes:
+vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                               
+#define pb(x) push_back(x)
+#define mp make_pair
+#define Max(x,y,z) max(x,max(y,z))
+#define Min(x,y,z) min(x,min(y,z))
+#define fori(i,a,n) for(int i=a;i<n;i++)
+#define forj(j,b,n) for(int j=b;j<n;j++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define pi (3.141592653589)
+#define MOD 1000000007
+                      
+ll factorial(ll n){
+ll ans=1;
+for(int i=1; i<=n; i++)
+ans = (ans*i) % MOD;
+return ans;
+}
+                            
+bool isPrime(ll n){
+if (n <= 1)
+return false;
+if (n <= 3)
+return true;
+if (n % 2 == 0 || n % 3 == 0)
+return false;
+for (ll i = 5; i * i <= n; i = i + 6){
+if (n % i == 0 || n % (i + 2) == 0)
+return false;
+}
+return true;
+}
+                    
+//ITS ABOUT DRIVE, ITS ABOUT POWER!!
+//WE STAY HUNGRY, WE DEVOUR!!
+//AIM -> WORK -> ACHIEVE -> REPEAT Carpe Diem!!
+                
+signed main()
+{
+    
+ll n;cin>>n;
+ll a[1000001];
+//assign values from 1 to 6
+for(ll i=1;i<=6;i++){
+    a[i]=pow(2,i-1);
+}
+// as each value on dice ranges from 1 to 6 the answer of i will be the sum of the i-6 to i-1
+for(ll i=7;i<=n;i++){ //iterating from 7 to n
+    ll sum=0;
+    for(ll j=1;j<=6;j++){ // iteratin from i-1 to i-6
+        sum=(sum+a[i-j])%1000000007;
+        a[i]=sum;
+    }
+}
+cout<<a[n]<<endl; // printing the ans
+
+}
